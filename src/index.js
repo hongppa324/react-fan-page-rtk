87af7bdb987fef9da180a57f60aeea0d04102b53
Peter Hong <t2023-m0052@Peter-Hong-MacBook-Air.local>
2024-02-21 03:06:30 +0900

    [UPDATE] Router Setup finished

--- src/index.js
+++ src/index.js
@@ -3,11 +3,12 @@ import ReactDOM from "react-dom/client";
 import "./reset.css";
 import App from "./App";
 import { Provider } from "react-redux";
+import store from "./redux/config/configStore";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <Provider>
+  <Provider store={store}>
     <App />
   </Provider>
 );
